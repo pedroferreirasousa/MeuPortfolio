735bc2c2037141ed9a22bd97b5746d3a723aa5e5
pedroferreirasousa <pepeferreiras.pf@gmail.com>
2025-04-04 07:11:38 -0300

    subindo projeto

---
+++ components/header/header.jsx
@@ -0,0 +1,22 @@
+
+
+const Header = () => {
+    return (
+        <header>
+            <div className="nav-container">
+            <h1><span>&lt;</span>Pedro Ferreira <span>&gt;</span></h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Início</a></li>
+                    <li><a href="#">Sobre mim</a></li>
+                    <li><a href="#">Habilidades</a></li>
+                    <li><a href="#">Projetos</a></li>
+                    <li><a href="#">Contato</a></li>
+                </ul>
+            </nav>
+            </div>
+        </header>
+    )
+}
+
+export default Header;

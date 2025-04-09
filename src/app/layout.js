21ebabac7fb2e0a68861ac7c7235d5875c0a032f
pedroferreirasousa <pepeferreiras.pf@gmail.com>
2025-04-09 13:56:55 -0300

    fixing ios mobile themess

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,6 +17,7 @@ export default function RootLayout({ children }) {
       <head>
         <meta name="theme-color" content={metadata.themeColor} />
         <meta name="apple-mobile-web-app-status-bar-style" content={metadata.appleMobileWebAppStatusBarStyle} />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body>
         <Header/>

9a705b92ae935f7eb98078a5c7953e2fcbffa946
pedroferreirasousa <pepeferreiras.pf@gmail.com>
2025-05-20 02:40:57 -0300

    atualizando componente "Projetos" e seus estilos

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -3,65 +3,53 @@
 import style from "../../styles/Projects/projects.scss";
 import ProjectComponent from "./ProjectCard/Projectcard";
 import atlasMercantil from "../../../public/media/AtlasMercantil.png";
+import cecle from "../../../public/media/Cecle.png";
+import Studia from "../../../public/media/Studia.png";
 
 
 
 const Projects = () => {
 
-    const cards = [{
-        name: "AtlasMercantil",
-        image: atlasMercantil,
-        description: "description",
-        url: "https://pedroferreirasousa.github.io/AtlasMercantil-2.0/",
-        urlcode: "https://github.com/pedroferreirasousa/AtlasMercantil-2.0"
-    },{
-        name: "AtlasMercantil",
-        image: atlasMercantil,
-        description: "description",
-        url: "url"
-    }, {
-        name: "AtlasMercantil",
-        image: atlasMercantil,
-        description: "description",
-        url: "url"
-    }, {
-        name: "AtlasMercantil",
-        image: atlasMercantil,
-        description: "description",
-        url: "url"
-    }, {
-        name: "AtlasMercantil",
-        image: atlasMercantil,
-        description: "description",
-        url: "url"
-    }, {
-        name: "AtlasMercantil",
-        image: atlasMercantil,
-        description: "description",
-        url: "url"
-    },
-    ]
+  const cards = [{
+    name: "CECLE",
+    image: cecle,
+    description: "Projeto feito para um Centro Espirita Kardecista de Uberlandia, feito utilizando Next.js, Typescript, Sass, Gsap...",
+    url: "https://cecle.vercel.app",
+  }, {
+    name: "Studia",
+    image: Studia,
+    description: "WebAplicativo de uma platforma para gerenciamento de estudos com IA, desenvolvido utilizando tecnologias como Next.js, TypeScript, Scss, ContextAPI etc... ",
+    url: "url",
+    urlcode: "https://github.com/pedroferreirasousa/Plataforma-de-Estudos-com-IA"
+  }, {
+    name: "AtlasMercantil",
+    image: atlasMercantil,
+    description: "Landing page com foco academico, desenvolvido com react + styledcomponents",
+    url: "https://pedroferreirasousa.github.io/AtlasMercantil-2.0/",
+    urlcode: "https://github.com/pedroferreirasousa/AtlasMercantil-2.0"
+  },
+  ]
 
 
-    return (
-        <section className="section-projects" id="projects">
-            <h1>Projetos</h1>
-            <div className="container-projects">
+  return (
+    <section className="section-projects" id="projects">
+      <h1>Projetos</h1>
+      <div className="container-projects">
 
-                {cards.map((cards, index) => (
-                    <ProjectComponent
-                        name={cards.name}
-                        image={cards.image}
-                        description={cards.description}
-                        url={cards.url}
-                        urlcode={cards.urlcode}
-                        key={index}
-                    />
-                ))}
-
-            </div>
-        </section>
-    )
+        {cards.map((cards, index) => (
+          <ProjectComponent
+            name={cards.name}
+            image={cards.image}
+            description={cards.description}
+            url={cards.url}
+            urlcode={cards.urlcode}
+            key={index}
+          />
+        ))}
+      </div>
+        <a className="linkGit" href="https://github.com/pedroferreirasousa?tab=repositories">Acessar repositórios do github</a>
+    </section>
+  )
 }
 
 export default Projects;
